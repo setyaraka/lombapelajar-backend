2062d7fd958e55db7f499324f64d3342936472bb
setya.raka <setya.raka@elefin.id>
2026-02-20 10:43:54 +0700

    update cors

--- src/app.js
+++ src/app.js
@@ -10,15 +10,27 @@ import adminRoutes from './routes/admin.routes.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
-app.use(express.json());
+/* ================= CORS ================= */
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
 
+app.use(cors({
+  origin: FRONTEND_URL,
+  credentials: true
+}));
+
+/* ================= PARSER ================= */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+/* ================= ROUTES ================= */
 app.use('/auth', authRoutes);
 app.use('/competitions', competitionRoutes);
 app.use('/registrations', registrationRoutes);
 app.use('/payments', paymentRoutes);
 app.use('/admin', adminRoutes);
 
+/* ================= STATIC FILE ================= */
 app.use('/uploads', express.static('uploads'));
 
+/* ================= SERVER ================= */
 app.listen(3000, () => console.log('API running on port 3000'));
